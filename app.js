1688dca0354aead8d7d9db65786e0b814f2a719c
mika <mika.morsky@gmail.com>
2020-03-31 23:01:50 +0300

    origin change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(body_parser.urlencoded({
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Content-Type", "application/json");
     next();
